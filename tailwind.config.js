1041a1b0be2b7fad0f274161bd7c1cdea8167218
ganeshpop <G@nE$h@!999>
2024-01-15 19:34:21 +0530

    Replace Main Page with Auction UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+// const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./app/templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
+      colors: {
+        'dpl-blue': '#050d1a',
+        'dpl-maroon': '#8C0D2F',
+        'dpl-red': '#BF1523'
+      },
+      fontFamily: {
+        teko: ["Teko Bold"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [plugin(function ({ matchUtilities, theme }) {
+    matchUtilities(
+      {
+        'text-shadow': (value) => ({
+          textShadow: value,
+        }),
+      },
+      { values: theme('textShadow') }
+    )
+  }),],
 }
 
